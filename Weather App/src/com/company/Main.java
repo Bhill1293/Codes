6c7d1ab557ee41c76376482c4a3f71ebf8cd5737
Brandon Hill <brandona_hill@yahoo.com>
2022-03-19 02:14:13 -0400

    Weather App
    App that will read input to determine current and weekly weather conditions. Will eventually use device IP to show data.

---
+++ Weather App/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	@TODO 1. Create Temperature Convertor
+	      2. Get User input
+	      3. Connect weather to locations
+	      4. Display weather info
+	 */
+
+    }
+}
