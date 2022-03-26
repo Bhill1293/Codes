18064ad8507d0eefbf90c2b371549fcf4d016099
Brandon Hill <brandona_hill@yahoo.com>
2022-03-25 22:59:12 -0400

    Zillow Clone
    Started with Programming w/ Mosh as practice will add API, and GUI to elevate challenge level

--- Weather App/src/com/company/Main.java
+++ Weather App/src/com/company/Main.java
@@ -1,7 +1,6 @@
 package com.company;
 
 public class Main {
-
     public static void main(String[] args) {
 	/*
 	@TODO 1. Create Temperature Convertor
@@ -9,6 +8,6 @@ public class Main {
 	      3. Connect weather to locations
 	      4. Display weather info
 	 */
-
+        
     }
 }
---
+++ mortgageCalculator/src/com/company/Main.java
@@ -0,0 +1,42 @@
+package com.company;
+
+
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	//* Excercise based on Programming With Mosh, Java tutorial /*
+
+        /*@TODO
+            ELEVATING THE PROGRAM
+            1.Simple mortgage calculations with input
+            2.GUI using JAVAFX
+            3.API connection to search for houses
+         */
+
+        /* NUMBERS NEEDED - PRINCIPAL, ANNUAL INTEREST, PERIOD, MORTGAGE
+         */
+
+    getMortgage();
+
+    }
+    static void getMortgage() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Principal: ");
+        int principal = scanner.nextInt();
+        System.out.println("Annual Interest Rate: ");
+        float interest = scanner.nextFloat();
+        System.out.println("Period (Years): ");
+        byte period = scanner.nextByte();
+        int numberOfPayments = period * 12;
+
+        float monthlyInterest = interest/100/12;
+
+        double mortgage = principal * (monthlyInterest * Math.pow( 1 + monthlyInterest,numberOfPayments) / (Math.pow( 1 + monthlyInterest,numberOfPayments)- 1));
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
+        System.out.println("Mortgage: " + mortgageFormatted);
+    }
+}
