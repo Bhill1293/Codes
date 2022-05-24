5eb0ae0564f553fc4ebe63b05e3ba0959745cf64
Brandon Hill <brandona_hill@yahoo.com>
2022-05-23 20:33:19 -0400

    Gui progress

--- mortgageCalculator/src/Main.java
+++ mortgageCalculator/src/Main.java
@@ -2,32 +2,44 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.text.NumberFormat;
 import java.util.Scanner;
+//* Excercise based on Programming With Mosh, Java tutorial /*
 
+/*@TODO
+    ELEVATING THE PROGRAM
+    1.Simple mortgage calculations with input *(COMPLETED)
+    2.GUI using JAVAFX *(IN PROGRESS)
+     ̶̶ ̶3̶.̶A̶P̶I̶ ̶c̶o̶n̶n̶e̶c̶t̶i̶o̶n̶ ̶t̶o̶ ̶s̶e̶a̶r̶c̶h̶ ̶f̶o̶r̶ ̶h̶o̶u̶s̶e̶s̶
+
+ */
+/* NUMBERS NEEDED - PRINCIPAL, ANNUAL INTEREST, PERIOD, MORTGAGE
+ */
 public class Main extends Application {
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+      try {
         Parent root = FXMLLoader.load(getClass().getResource("resources/sample.fxml"));
-        Scene home = new Scene(root);
-        primaryStage.setScene(home);
+        Scene scene = new Scene(root);
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(500);
+
+        ImageView logo = new ImageView("https://www.clipartmax.com/png/middle/4-45061_house-png-house-logo-clipart.png");
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Mortgage Calculator");
+
         primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
+      }
     }
 
     public static void main(String[] args) {
-        //* Excercise based on Programming With Mosh, Java tutorial /*
-
-        /*@TODO
-            ELEVATING THE PROGRAM
-            1.Simple mortgage calculations with input *(COMPLETED)
-            2.GUI using JAVAFX *(IN PROGRESS)
-            3.API connection to search for houses
-         */
-
-        /* NUMBERS NEEDED - PRINCIPAL, ANNUAL INTEREST, PERIOD, MORTGAGE
-         */
 
 //        getMortgage();
         launch(args);
--- mortgageCalculator/src/controllers/Controller.java
+++ mortgageCalculator/src/controllers/Controller.java
@@ -1,10 +1,27 @@
 package controllers;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
 
 public class Controller {
     FXMLLoader fxmlLoader = new FXMLLoader();//creating FXMLLoader object
 //    fxmlLoader.setLocation(new URL("com.company/sample.fxml"));//accessing FXML file
+
+    private float interest;
+    private int principal;
+    private byte period;
+
+
+
+
+    @FXML
+    public void operateButton (ActionEvent click) {
+     Button button = (Button)click.getSource();
+
+    }
 
 
 }
